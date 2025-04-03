633b6201962aeb4236bc8408ca24b496cfa6b381
KimEunJi23 <yoonsdmswl4786@gmail.com>
2025-04-03 23:59:46 +0900

    week03_김은지

--- week03/김은지/LottoMachineTest.java
+++ week03/김은지/LottoMachineTest.java
@@ -1,79 +1,8 @@
-package likelion;
-
-//로또 번호를 뽑는 기계
-
-import java.util.Scanner;
-
-class LottoMachine {
-    //6개 로또 번호 변수
-    Scanner scn = new Scanner(System.in);
-    private int[] lottoNumber = new int[6];
-
-    void drawNumbers() {
-        for (int i = 0; i < lottoNumber.length; i++) {
-            lottoNumber[i] = scn.nextInt();//로또 번호 생성
-        }
-    }
-
-    int[] getWinningNumbers() {
-        return lottoNumber;
-    }
-
-
-}
-
-//플레이어가 로또 번호 입력
-class Player {
-    Scanner scn = new Scanner(System.in);
-    private int[] playerNumbers = new int[6];
-
-    void inputNumbers() {
-        for (int i = 0; i < playerNumbers.length; i++) {
-            playerNumbers[i] = scn.nextInt();
-        }
-    }
-
-    int[] getPlayerNumbers() {
-        return playerNumbers;
-    }
-
-}
-
-//로또 번호가 맞았는지 확인하는 클래스
-class LottoChecker {
-    public String checkResult(int[] winningNumbers, int[] playerNumbers) {
-        int num = 0;
-        String result;
-        for (int i = 0; i < winningNumbers.length; i++) {
-            for (int j = 0; j < winningNumbers.length; j++) {
-                if (winningNumbers[i] == playerNumbers[j]) {
-                    num += 1;
-                }
-
-            }
-        }
-        switch (num) {
-            case 5,6:
-                result = "1등";
-                break;
-            case 4:
-                result = "2등";
-                break;
-            case 3:
-                result = "3등";
-                break;
-            default:
-                result = "꽝";
-        }
-
-        return result;
-    }
-
-}
 
 public class LottoMachineTest {
-    public static void main(String[] args) {
-        LottoMachine lottoMachine = new LottoMachine();
+
+	public static void main(String[] args) {
+		LottoMachine lottoMachine = new LottoMachine();
         lottoMachine.drawNumbers();
 
         Player player = new Player();
@@ -86,5 +15,6 @@ public class LottoMachineTest {
         String result = lottoChecker.checkResult(winningNumbers, playerNumbers);
         System.out.println("결과: " + result);
 
-    }
+	}
+
 }
