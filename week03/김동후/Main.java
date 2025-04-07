aecf56bd719340e74545074f7d8f06ab2e4b59e5
donghoo5851 <donghoo1106@naver.com>
2025-04-07 12:30:05 +0900

    week03_김동후

--- week03/김동후/Main.java
+++ week03/김동후/Main.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,53 +18,5 @@ public class Main {
 
         System.out.println();
         System.out.println("결과: " + result);
-    }
-}
-
-class LottoMachine {
-    private int[] winningNumbers = new int[6];
-
-    public void drawNumbers(Scanner scanner) {
-        for (int i = 0; i < 6; i++) {
-            winningNumbers[i] = scanner.nextInt();
-        }
-        Arrays.sort(winningNumbers);
-    }
-
-    public int[] getWinningNumbers() {
-        return winningNumbers;
-    }
-}
-
-class Player {
-    private int[] playerNumbers = new int[6];
-
-    public void inputNumbers(Scanner scanner) {
-        for (int i = 0; i < 6; i++) {
-            playerNumbers[i] = scanner.nextInt();
-        }
-        Arrays.sort(playerNumbers);
-    }
-
-    public int[] getPlayerNumbers() {
-        return playerNumbers;
-    }
-}
-
-class LottoChecker {
-    public String checkResult(int[] winningNumbers, int[] playerNumbers) {
-        int matchCount = 0;
-        for (int wn : winningNumbers) {
-            for (int pn : playerNumbers) {
-                if (wn == pn) matchCount++;
-            }
-        }
-
-        switch (matchCount) {
-            case 5: return "1등";
-            case 4: return "2등";
-            case 3: return "3등";
-            default: return "꽝";
-        }
     }
 }
