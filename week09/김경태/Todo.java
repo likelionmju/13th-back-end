ca2ef226174350d891de5227264e23dc31ac3934
OpAue <kkt23917@naver.com>
2025-06-23 11:39:15 +0900

    김경태_week09

---
+++ week09/김경태/Todo.java
@@ -0,0 +1,25 @@
+package com.example.ll_week09;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private int Date;
+    private String Todo;
+    private String Done;
+}
