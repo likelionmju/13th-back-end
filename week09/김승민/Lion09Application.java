5fad9209d4618068a70119955e46b98b45091407
github seungmin0625 <seugmin1535@naver.com>
2025-06-23 14:20:34 +0900

    week09_김승민

---
+++ week09/김승민/Lion09Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lion09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lion09Application.class, args);
+	}
+
+}
