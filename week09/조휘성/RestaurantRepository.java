b57d27914745f31d3d9dc64841339455574c9913
조휘성 <hwiseongjo02@naver.com>
2025-06-25 00:29:31 +0900

    week09_조휘성

---
+++ week09/조휘성/RestaurantRepository.java
@@ -0,0 +1,10 @@
+package example.usecase.repository;
+
+import example.usecase.domain.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+}
